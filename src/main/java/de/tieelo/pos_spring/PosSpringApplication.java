29e7aa2d87850e86cb04fb291e527e464090fa1f
mirco.franzkowiak <mirco.franzkowiak@zurich.com>
2024-10-18 14:09:27 +0200

    init commit

---
+++ src/main/java/de/tieelo/pos_spring/PosSpringApplication.java
@@ -0,0 +1,13 @@
+package de.tieelo.pos_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosSpringApplication.class, args);
+	}
+
+}
