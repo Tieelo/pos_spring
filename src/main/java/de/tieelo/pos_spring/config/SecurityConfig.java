6111fc8d645e10942d03bfd6e2caf803fa9566e6
mirco.franzkowiak <mirco.franzkowiak@zurich.com>
2024-12-10 00:16:15 +0100

    Refactor CORS configuration and update database URL.
    Removed CorsConfig.java and integrated its functionality into SecurityConfig.java, centralizing the CORS setup. Updated application.properties to adjust CORS parameters and change the datasource URL. This refactoring improves the maintainability and simplicity of the security configuration setup.

--- src/main/java/de/tieelo/pos_spring/config/SecurityConfig.java
+++ src/main/java/de/tieelo/pos_spring/config/SecurityConfig.java
@@ -1,4 +1,5 @@
 package de.tieelo.pos_spring.config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,37 +11,46 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.List;
+
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
 
-	private final CorsConfigurationSource corsConfigurationSource;
-	public SecurityConfig(CorsConfigurationSource corsConfigurationSource) {
-		this.corsConfigurationSource = corsConfigurationSource;
-	}
+	@Value("${api.cors.allowedOrigins}")
+	private String allowedOrigins;
 
-//	@Bean
-//	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//		http
-//				.authorizeHttpRequests(authorize -> authorize
-//						.anyRequest().permitAll() // Alle Anfragen ohne Authentifizierung zulassen
-//				)
-//				.csrf(csrf -> csrf.disable()); // CSRF-Schutz deaktivieren, falls benötigt
-//
-//		return http.build();
-//	}
+	@Value("${api.cors.allowedMethods}")
+	private String allowedMethods;
+
+	@Value("${api.cors.allowedHeaders}")
+	private String allowedHeaders;
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
 				.csrf(AbstractHttpConfigurer::disable)
-				.cors(cors -> cors.configurationSource(corsConfigurationSource))
+				.cors(cors -> cors.configurationSource(corsConfigurationSource()))
 				.authorizeHttpRequests(authorize -> authorize
 						.anyRequest().authenticated()
 				)
 				.addFilterBefore(new ApiKeyFilter(), UsernamePasswordAuthenticationFilter.class);
 
 		return http.build();
+	}
+
+	@Bean
+	public UrlBasedCorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(List.of(allowedOrigins));
+		configuration.setAllowedMethods(List.of(allowedMethods));
+		configuration.setAllowedHeaders(List.of(allowedHeaders));
+		configuration.setAllowCredentials(true);
+//		configuration.setMaxAge(3600L);
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/api/**", configuration);
+		return source;
 	}
 
 }
