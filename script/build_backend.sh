59e791883327174914f3d4f806ca0953b4739782
mirco.franzkowiak <mirco.franzkowiak@zurich.com>
2024-12-09 14:23:48 +0100

    Migrate database to PostgreSQL and integrate Docker
    Remove SQLite and add PostgreSQL configurations, updating database settings. Introduce Docker support with new build and deployment scripts, along with a Dockerfile for containerization. Implement semantic versioning with initial setup in `semver.txt`.

---
+++ script/build_backend.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -o errexit
+
+source .secrets # enthält die Variablen DOCKER_USERNAME, DOCKER_PASSWORD und GITHUB_TOKEN
+export BRANCH="develop"
+export APPLICATION="swe_pos_backend"
+export USER="tieelo"
+
+git clone --branch=${BRANCH} https://${GITHUB_USERNAME}:${GITHUB_TOKEN}@github.com/${USER}/${APPLICATION}.git
+cd ${APPLICATION}
+
+export VERSION="$(grep -m 1 "^#" semver.txt | cut -c 2- | tr -d '\r')"
+export TAG="${USER}/${APPLICATION}:${VERSION}"
+export LATEST="${USER}/${APPLICATION}:latest"
+
+mvn clean install
+
+cp ./target/${APPLICATION}-${VERSION}.jar ./deployment/app.jar
+
+echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
+docker build -f Dockerfile --no-cache -t ${TAG} .
+docker tag ${TAG} ${LATEST}
+docker push ${LATEST}
+docker push ${TAG}
+docker logout
+cd ..
+rm -fr ${APPLICATION}/
